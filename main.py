6464077019aa5c6eb55d83df7b64ee1db8c80d22
elprofesoriqo <igorjankowwski@gmail.com>
2024-10-16 22:54:20 +0200

    Main chat

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-from fastapi import FastAPI, status, Depends, HTTPException
+from email import message
+from sqlite3 import Connection
+
+from fastapi import FastAPI, status, Depends, HTTPException, WebSocket
+from sqlalchemy.orm import Session
+from typing import Annotated
+
+from starlette.websockets import WebSocketDisconnect
 
 import auth
 import models
-
 from database import engine, SessionLocal
-from typing import Annotated
-from sqlalchemy.orm import Session
 from auth import get_current_user
 
 app = FastAPI()
 
 app.include_router(auth.router)
-
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -22,12 +25,68 @@ def get_db():
     finally:
         db.close()
 
-
 db_dependency = Annotated[SessionLocal, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[Connection] = []
+        async def connect(self, websocket: WebSocket):
+            await websocket.accept()
+            self.active_connections.append(websocket)
+        def disconnect(self, websocket: WebSocket):
+            self.active_connections.remove(websocket)
+
+        async def send_personal_message(self, message: str, websocket: WebSocket):
+            await websocket.send_text(message)
+
+        async def broadcast(self, message: str):
+            for connection in self.active_connections:
+                await connection.send_text(message)
+
+manager = ConnectionManager()
+
+
+
 @app.get("/", status_code=status.HTTP_200_OK)
-async def user(user: user_dependency, db: db_dependency):
-    if user is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+async def read_user(user: user_dependency, db: db_dependency):
     return {'user': user}
+
+@app.get("/stock/{symbol}", status_code=status.HTTP_200_OK)
+async def stock_page(user: user_dependency, db: db_dependency):
+    pass
+
+
+@app.websocket("/chat/ws")
+async def websocket_endpoint(websocket: WebSocket, user: user_dependency, db: db_dependency):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+
+            chat_message = models.Chat(
+                user_id=user["id"],
+                title="Chat",
+                comment=data
+            )
+            db.add(chat_message)
+            db.commit()
+
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"User {user['username']} wrote: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+
+
+@app.get("/chat", status_code=status.HTTP_200_OK)
+async def get_chat_history(user: user_dependency, db: db_dependency):
+    chat_history = db.query(models.Chat).order_by(models.Chat.id).all()
+
+    return [
+        {
+            "user_id": chat_message.user_id,
+            "username": user["username"],
+            "message": chat_message.comment
+        }
+        for chat_message in chat_history
+    ]
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
+from sqlalchemy.orm import relationship
 from database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class Users(Base):
@@ -8,3 +9,29 @@ class Users(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, nullable=False)
     hashed_password = Column(String, nullable=False)
+
+    # Relacja z modelem Chat
+    chats = relationship("Chat", back_populates="user")
+
+
+class Stocks(Base):
+    __tablename__ = 'stocks'
+
+    id = Column(Integer, primary_key=True, index=True)
+    symbol = Column(String, unique=True, nullable=False)
+    name =  Column(String, nullable=False)
+    curr_price = Column(Integer, nullable=False)
+    yesterday_price = Column(Integer, nullable=False)
+    analyse =  Column(String, nullable=True)
+
+
+class Chat(Base):
+    __tablename__ = 'chat'
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    title = Column(String, nullable=False)
+    comment = Column(String, nullable=False)
+
+    # Relacja z modelem Users
+    user = relationship("Users", back_populates="chats")
